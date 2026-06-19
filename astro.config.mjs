5b98d9c8393ee5f25d4969fa2ce42cf11085f21e
Ian Engelbrecht <ianicus.za@gmail.com>
2026-06-19 21:15:51 +0200

    initial commit, first pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,26 +1,41 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
+import starlightThemeNova from 'starlight-theme-nova'
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [
-		starlight({
-			title: 'My Docs',
-			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/withastro/starlight' }],
-			sidebar: [
-				{
-					label: 'Guides',
-					items: [
-						// Each item here is one entry in the navigation menu.
-						{ label: 'Example Guide', slug: 'guides/example' },
-					],
-				},
-				{
-					label: 'Reference',
-					items: [{ autogenerate: { directory: 'reference' } }],
-				},
-			],
-		}),
-	],
+  integrations: [
+    starlight({
+      title: 'Herbarium Specimen Duplicate Finder',
+      favicon: '/favicon.png',
+      logo: {
+        src: './src/assets/logo.png',
+      },
+      defaultLocale: 'en',
+      locales: {
+        en: {
+          label: 'English',
+          lang: 'en',
+        },
+        fr: {
+          label: 'Français',
+          lang: 'fr',
+        },
+        pt: {
+          label: 'Português',
+          lang: 'pt',
+        },
+        mg: {
+          label: 'Malagasy',
+          lang: 'mg',
+        },
+      },
+      social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/withastro/starlight' }],
+
+      plugins: [
+        starlightThemeNova(/* options */),
+      ],
+    }),
+  ],
 });
---
+++ src/content/docs/en/installation.md
@@ -0,0 +1,8 @@
+---
+title: Installation
+description: Download and installation instructions
+---
+
+## Installation
+
+Here we will download and install the tool
